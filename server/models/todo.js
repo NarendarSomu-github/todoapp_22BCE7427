615cd85cfd3a36d2e68541218997ac284f95f023
SomuNarendar <narendar.s@email.com>
2024-06-28 23:32:22 -0400

    first commit

---
+++ server/models/todo.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+
+const todoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        default:'',
+    },
+
+    done: {
+        type: Boolean,
+        default:false,
+    }
+})
+
+module.exports = mongoose.model("todo", todoSchema)
+
